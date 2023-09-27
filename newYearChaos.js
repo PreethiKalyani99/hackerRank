756af4d2fb86b83d2b4bafe04f421c6c9ec8b031
Preethi-Kalyani <preethikalyani99@gmail.com>
2023-09-27 15:16:03 +0530

    Hacker rank - new year chaos

--- newYearChaos.js
+++ newYearChaos.js
@@ -1,11 +1,12 @@
 function minimumBribes(q){
     let bribeCount = 0
-
+    let index 
     for(let i = q.length-1; i >= 0; i--){
+        index = i-2
         if((q[i] - i) > 3){
             return "Too chaotic"
         }
-        for(let j = Math.max(0, q[i] - 2); j < i; j++) {
+        for(let j = (index < 0 ? 0 : index); j < i; j++) {
             if (q[j] > q[i]) {
                 bribeCount++;
             }
@@ -13,4 +14,4 @@ function minimumBribes(q){
     }
     return bribeCount;
 }
-console.log(minimumBribes([2,5,1,3,4])); 
+console.log(minimumBribes([1, 2, 5, 3, 7, 8, 6, 4])); 
