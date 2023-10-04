0a8e4d99f3794c318bd7999a695685cdfd9e2ec0
Preethi-Kalyani <preethikalyani99@gmail.com>
2023-10-04 13:30:13 +0530

    added new function 'pizzaMenu'

--- pizzaPriceCalculator.js
+++ pizzaPriceCalculator.js
@@ -1,20 +1,3 @@
-// const {pizzaMenu, toppings} = require('./variables/constants')
-
-let pizzaMenu = {
-    'veg': {
-        'pepper paneer': {'small' : 120, 'medium': 220, 'large': 350},
-        'Mexican green wave' : {'small' : 140, 'medium': 250, 'large': 380},
-        'cheese N corn': {'small' : 130, 'medium': 240, 'large': 350},
-        'tandoori paneer': {'small' : 150, 'medium': 250, 'large': 370}
-    }, 
-    'nonveg':{
-        'chicken golden delight': {'small' : 120, 'medium': 220, 'large': 350},
-        'chicken dominator': {'small' : 150, 'medium': 250, 'large': 380},
-        'double cheese chicken': {'small' : 200, 'medium': 350, 'large': 500},
-        'pepper barbeque chicken': {'small' : 190, 'medium': 300, 'large': 450} 
-    }
-}
-
 let toppings = {
     'extra cheese' : {'small' : 20, 'medium': 40, 'large': 60},
     'tomato': {'small' : 20, 'medium': 40, 'large': 60},
@@ -22,18 +5,47 @@ let toppings = {
     'onions': {'small' : 20, 'medium': 40, 'large': 60},
     'extra chicken': {'small' : 20, 'medium': 40, 'large': 60}
 }
+const pizzaData = [
+    { pizzaType: 'nonveg', pizzaName: 'paneer chicken', smallPrice: 140, mediumPrice: 240, largePrice: 380 },
+    { pizzaType: 'nonveg', pizzaName: 'chicken dominator', smallPrice: 150, mediumPrice: 250, largePrice: 390 },
+    { pizzaType: 'nonveg', pizzaName: 'double cheese chicken', smallPrice: 200, mediumPrice: 350, largePrice: 490 },
+    { pizzaType: 'veg', pizzaName: 'Mexican green wave', smallPrice: 120, mediumPrice: 220, largePrice: 350 },
+    { pizzaType: 'veg', pizzaName: 'cheese N corn', smallPrice: 130, mediumPrice: 240, largePrice: 370 },
+    { pizzaType: 'veg', pizzaName: 'tandoori paneer', smallPrice: 150, mediumPrice: 250, largePrice: 370 },
+]
 
+function pizzaMenu(pizzaData){
+    let menuCard = {}
+    for(const pizza of pizzaData){
+        // console.log(pizza);
+        const {pizzaType, pizzaName, smallPrice, mediumPrice, largePrice} = pizza
+         if(!menuCard[pizzaType]){
+            menuCard[pizzaType] = {}
+        }
+        if(!menuCard[pizzaType][pizzaName]){
+            menuCard[pizzaType][pizzaName] = {}
+        }
 
-function validateOrder({pizzaType, pizza, size, toppingsList}){
+        menuCard[pizzaType][pizzaName]['small'] = smallPrice
+        menuCard[pizzaType][pizzaName]['medium'] = mediumPrice
+        menuCard[pizzaType][pizzaName]['large'] = largePrice
+    }
+    return menuCard
+}
+
+let pizzaMenuCard = pizzaMenu(pizzaData)
+
+function validateOrder(pizzaType, pizza, size, toppingsList){
+
     const unavailableToppings = toppingsList.filter(topping => !toppings[topping])
-
-    if(!pizzaMenu[pizzaType]){
+    
+    if(!pizzaMenuCard[pizzaType]){
         return {isValid: false, reason : `PizzaType "${pizzaType}" is not available`}
     }
-    if(!pizzaMenu[pizzaType][pizza]){
+    if(!pizzaMenuCard[pizzaType][pizza]){
         return {isValid: false, reason : `"${pizza}" pizza is not available`}
     }
-    if(!pizzaMenu[pizzaType][pizza][size]){
+    if(!pizzaMenuCard[pizzaType][pizza][size]){
         return {isValid: false, reason : `"${size}" size is not available`}
     }
     if(unavailableToppings.length > 0){
@@ -42,23 +54,35 @@ function validateOrder({pizzaType, pizza, size, toppingsList}){
     return {isValid: true, reason : ''}
 }
 
-function pizzaPriceCalculator(pizzaType, pizza, size, toppingsList){
-   let result = validateOrder({pizzaType, pizza, size, toppingsList})
-
-   if(!result.isValid){
-      return result.reason
-   }
-    const toppingsTotal = toppingsList.reduce((acc,cur) =>{
-        acc += toppings[cur][size]
-        return acc
-    },0)
-
-    const total = pizzaMenu[pizzaType][pizza][size] + toppingsTotal
-    return `Your order is: ${pizzaType} - ${pizza} pizza ${size} with topping(s) ${toppingsList} and your total is: ${total}`
+function pizzaPriceCalculator(pizzaOrder){
+    let orderDetails = [];
+    let total = 0
+    for(const items of pizzaOrder){
+        const {pizzaType, pizza, size, toppingsList} = items
+        // console.log(pizzaType, pizza, size, toppingsList);
+        let result = validateOrder(pizzaType, pizza, size, toppingsList)
+        
+        if(!result.isValid){
+            return result.reason
+        }
+        let toppingsTotal = 0
+        if(toppingsList){
+            toppingsTotal += toppingsList.reduce((acc,cur) =>{
+                acc += toppings[cur][size]
+                return acc
+            },0)
+            
+        }
+        
+        const pizzaPrice = pizzaMenuCard[pizzaType][pizza][size]
+        total = pizzaPrice + toppingsTotal + total;
+        orderDetails.push(`${pizzaType} - ${pizza} pizza ${size} with toppings ${toppingsList}`);
+    }
+    return `Your order details are: ${orderDetails.join(' & ')} and the total is: ${total}`;
 }
 
-console.log(pizzaPriceCalculator('v','Mexican green wave', 'large', ['extra chicken', 'black olives']));
-console.log(pizzaPriceCalculator('veg','Mexican  wave', 'large', ['extra chicken', 'black olives']));
-console.log(pizzaPriceCalculator('veg','Mexican green wave', 'huge', ['extra chicken', 'black olives']));
-console.log(pizzaPriceCalculator('veg','Mexican green wave', 'large', ['potato', 'black olives']));
-console.log(pizzaPriceCalculator('nonveg','double cheese chicken', 'large', ['extra chicken', 'black olives']));
+const pizzaOrder = [
+    {pizzaType: 'veg', pizza: 'Mexican green wave', size: 'small', toppingsList: ['extra chicken', 'black olives']},
+    {pizzaType: 'nonveg', pizza: 'paneer chicken', size: 'large', toppingsList: ['extra chicken', 'black olives']}
+]
+console.log(pizzaPriceCalculator(pizzaOrder));
