fb8f4dd88f11d8d36d7de41fb85520b223553351
Preethi-Kalyani <preethikalyani99@gmail.com>
2023-10-18 18:00:33 +0530

    modified Hacker rank sales match

--- salesMatch.js
+++ salesMatch.js
@@ -1,10 +1,17 @@
 function salesMatch(arr){
-    let pairCount = {}
+    let pairCount = {}, count = 0
 
     arr.reduce((count, currentvalue) => {
        pairCount[currentvalue] = (pairCount[currentvalue] || 0) + 1
+    },0)
+
+
+    Object.values(pairCount).filter(num => {
+       if(num > 1){
+        count += Math.floor(num / 2)
+       }
     })
-    return pairCount
+    return count 
 }
 
 console.log(salesMatch([10,20,30,10,20,50,20,30,70]));
