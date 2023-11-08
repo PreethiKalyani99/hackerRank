0cb97f69fd9547bbc36c171c90a7482d630077be
Preethi-Kalyani <preethikalyani99@gmail.com>
2023-11-08 12:30:25 +0530

    Hacker rank - roads and libraries using bfs algorithm

--- roadsAndLibraries.js
+++ roadsAndLibraries.js
@@ -1,65 +1,82 @@
-function isVisited(cityArr, startValue, endValue){
-    let start = 'No', end = 'No'
-    for(let i = 0; i < cityArr.length; i++){
-        if(startValue === cityArr[i].start || startValue === cityArr[i].end){
-            start = 'Yes'
-        }
-        if(endValue === cityArr[i].end){
-            end = 'Yes'
-        }
-        if(start === 'Yes' && endValue === cityArr[i].start){
-            end = 'Yes'
+function findConnectedComponents(graph) {
+    let visited = new Set();
+    let components = [];
+  
+    function bfs(start, component) {
+        let queue = [start];
+        visited.add(start);
+      
+        while (queue.length > 0) {
+            let vertex = queue.shift();
+            component.push(vertex);
+      
+            for (let neighbor of graph.get(vertex)) {
+                if (!visited.has(neighbor)) {
+                    queue.push(neighbor);
+                    visited.add(neighbor);
+                }
+            }
         }
     }
-    return [start,end]
+    for (let vertex of graph.keys()) {
+        if (!visited.has(vertex)) {
+            let component = [];
+            bfs(vertex, component);
+            components.push(component);
+        }
+    }
+    return components;
 }
+
 
 function roadsAndLibraries(n, cl, cr, cities){
     if(cr >= cl){
         return n * cl
     }
     if(cities.length === 0){
-        // console.log(cl, n)
         return cl * n
     }
-    let cityArr = [], roadCount = 0
-
-    cities.forEach(([startValue, endValue]) => {
-        
-        if(cityArr.length === 0){
-            cityArr.push({ start: startValue, end: endValue });
-            roadCount++
-        }
-        else{
-            let result = isVisited(cityArr, startValue, endValue)
-            if((result[0] === 'No' && result[1] === 'No') || (result[0] === 'No' && result[1] === 'Yes') || (result[0] === 'Yes' && result[1] === 'No') ){
-                cityArr.push({ start: startValue, end: endValue })
-                roadCount++
-            }
-        }
-    });
-
-    let values = [].concat(...cityArr.map(item => Object.values(item)))
+    
+    let values = [].concat(...cities)
     const valueCounts = {}
+
 
     for (let value of values) {
         valueCounts[value] = (valueCounts[value] || 0) + 1
     }
 
+   let adjacencyList = new Map();
+    cities.forEach(item => {
+        if (adjacencyList.has(item[0])) {
+            adjacencyList.get(item[0]).push(item[1]);
+        } else {
+            adjacencyList.set(item[0], [item[1]]);
+        }
+    
+        if (adjacencyList.has(item[1])) {
+            adjacencyList.get(item[1]).push(item[0]);
+        } else {
+            adjacencyList.set(item[1], [item[0]]);
+        }
+    });
+    
+  
+    let connectedComponents = findConnectedComponents(adjacencyList)
+    let roads = 0
+
+    connectedComponents.forEach(item => {
+        roads += item.length - 1
+    })
+
+    let libraryCount = connectedComponents.length
     let cityWithConnection = Object.keys(valueCounts)
     let cityWithNoConnection = 0
 
     if(n !== cityWithConnection.length){
         cityWithNoConnection = n - cityWithConnection.length
     }
-    // let libraryCount = Object.values(valueCounts).filter(val => {return val > 1})
-    // console.log(roadCount, cr, libraryCount.length,cl);
-    // console.log(cityArr)
-    
-    // console.log(libraryCount )
-    // console.log(cityWithConnection,'connection')
-    // return (roadCount * cr) + (libraryCount.length * cl)  + (cityWithNoConnection * cl)
-    return (roadCount * cr) + (cityWithNoConnection * cl)
+
+    return (roads * cr) + (libraryCount * cl)  + (cityWithNoConnection * cl)
 }
-// console.log(roadsAndLibraries(5,92,23, [[2,1],[5,3],[5,1],[3,4],[3,1],[5,4],[4,1],[5,2],[4,2]]))
-console.log(roadsAndLibraries(5, 2, 1, [[1,2],[1,3],[4,5]]))
+
+console.log(roadsAndLibraries(5, 2,1, [[1,2],[1,3],[2,3], [4,5]]))
